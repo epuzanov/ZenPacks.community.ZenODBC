009c70b28100b59e7baebd9c09dda298d3205080
Egor Puzanov <epuzanov@gmx.de>
2012-03-20 20:41:47 +0100

    SQLDataSource 2.0 support

--- ZenPacks/community/ZenODBC/OdbcPlugin.py
+++ ZenPacks/community/ZenODBC/OdbcPlugin.py
@@ -1,7 +1,7 @@
 ################################################################################
 #
 # This program is part of the ZenODBC Zenpack for Zenoss.
-# Copyright (C) 2009, 2010, 2011 Egor Puzanov.
+# Copyright (C) 2009-2012 Egor Puzanov.
 #
 # This program can be used under the GNU General Public License version 2
 # You can find full information here: http://www.zenoss.com/oss
@@ -12,9 +12,9 @@ __doc__="""OdbcPlugin
 
 wrapper for PythonPlugin
 
-$Id: OdbcPlugin.py,v 2.0 2011/05/03 22:20:29 egor Exp $"""
+$Id: OdbcPlugin.py,v 2.1 2012/03/20 20:22:48 egor Exp $"""
 
-__version__ = "$Revision: 2.0 $"[11:-2]
+__version__ = "$Revision: 2.1 $"[11:-2]
 
 from ZenPacks.community.SQLDataSource.SQLPlugin import SQLPlugin
 
@@ -26,7 +26,9 @@ class OdbcPlugin(SQLPlugin):
             if len(query) == 3:
                 cs, sql, columns = query
                 kbs = {}
-                if type(columns) is not dict:
+                if type(columns) is dict:
+                    columns = dict(zip(columns.values(), columns.keys()))
+                else:
                     columns = dict(zip(columns, columns))
             else:
                 sql, kbs, cs, columns = query 
