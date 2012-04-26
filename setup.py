f9a4b2fec8478ac70564a4dfe301114d1bc125c4
Egor Puzanov <epuzanov@gmx.de>
2012-04-27 00:07:57 +0200

    4.0 release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.ZenODBC"
-VERSION = "3.91"
+VERSION = "4.0"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.ZenODBC']
-INSTALL_REQUIRES = ['ZenPacks.community.SQLDataSource>=1.99']
+INSTALL_REQUIRES = ['ZenPacks.community.SQLDataSource>=2.0']
 COMPAT_ZENOSS_VERS = ">=2.5"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
